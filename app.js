26924c84497252239dfaa71e09b4e5c6ebccd58d
Migue <miguelulmi@hotmail.com>
2024-08-22 15:19:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ function enterValue(){
     value[1] = prompt("Ingrese el segundo valor");
 }
 
+switch (operation){
+    case "suma":
+        value[3] = value[1] + value[2];
+        break;
+    case "resta":
+        value[3] = value[1] - value[2];
+        break;
+    case "multiplicación":
+        value[3] = value[1] * value[2];
+        break;
+    case "división":
+        value[3] = value[1] / value[2];
+        break;
+    default:
+        alert(`¡Hasta la próxima!`);
+}
+
+function suma(value1, value2){
+    return value1 + value2;
+}
+
 function printResult(operation, result){
     alert(`El resultado de la ${operation} es ${result}`);
 }
