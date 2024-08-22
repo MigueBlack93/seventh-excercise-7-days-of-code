1004eb0064dece87e913899ed4509ad469e8ceb2
Migue <miguelulmi@hotmail.com>
2024-08-22 14:37:42 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+let valor = [];
+let operation = ["suma", "resta", "división", "multiplicación"];
+
+function printResult(result, operation){
+    alert(`El resultado de la ${operation} es ${result}`);
+}
+
