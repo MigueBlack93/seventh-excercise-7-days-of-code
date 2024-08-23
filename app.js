2c661478aa308aba1b37edf6ae8f9519aa5c6441
Migue <miguelulmi@hotmail.com>
2024-08-22 22:14:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
 let value = [];
 let operation = 0;
 
-function enterValue(){
-    operation = prompt("Ingrese tipo de operación");
+
+operation = prompt("Ingrese tipo de operación. Escriba suma, resta, multiplicación, división o salir");
+if(operation == "salir"){
+    alert(`¡Hasta la próxima!`);
+}else{
     value[0] = prompt("Ingrese el primer valor");
     value[1] = prompt("Ingrese el segundo valor");
+    switch (operation){
+        case "suma":
+            value[3] = suma(value[1], value[2]);
+            printResult(operation, value[3]);
+            break;
+        case "resta":
+            value[3] = resta(value[1], value[2]);
+            printResult(operation, value[3]);
+            break;
+        case "multiplicación":
+            value[3] = multiplicacion(value[1], value[2]);
+            printResult(operation, value[3]);
+            break;
+        case "división":
+            value[3] = division(value[1], value[2]);
+            printResult(operation, value[3]);
+            break;
+        default:
+            alert(`¡Hasta la próxima!`);
+    }
 }
 
-switch (operation){
-    case "suma":
-        value[3] = value[1] + value[2];
-        break;
-    case "resta":
-        value[3] = value[1] - value[2];
-        break;
-    case "multiplicación":
-        value[3] = value[1] * value[2];
-        break;
-    case "división":
-        value[3] = value[1] / value[2];
-        break;
-    default:
-        alert(`¡Hasta la próxima!`);
-}
 
 function suma(value1, value2){
     return value1 + value2;
+}
+
+function resta(value1, value2){
+    return value1 - value2;
+}
+
+function multiplicacion(value1, value2){
+    return value1 * value2;
+}
+
+function division(value1, value2){
+    return value1 / value2;
 }
 
 function printResult(operation, result){
